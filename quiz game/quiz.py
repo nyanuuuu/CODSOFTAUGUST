1368900735c9798671596d07c131bda8e39514db
nyanuuuu <144016503+nyanuuuu@users.noreply.github.com>
2023-09-22 21:20:55 +0530

    Add files via upload

---
+++ quiz game/quiz.py
@@ -0,0 +1,61 @@
+
+print("Welcome to the Quiz Game!")
+print("You will be asked a series of multiple-choice questions.")
+print("For each question, enter the letter corresponding to your answer (A, B, C, or D).")
+print("Let's begin!\n")
+
+play_again = "yes"
+
+while play_again.lower() == "yes":
+    questions = ("How many elements are in periodic table?",
+                 "Which animal lays the largest eggs?",
+                 "What is the most abundant gas in earth's atmosphere ?",
+                 "How many bones are in human body ?",
+                 "Which planet in the solar system is the hottest?")
+
+    options = (("A.116","B.177","C.118","D.119"),
+               ("A.Whale","B.Crocodile","C.Elephant","D.Ostrich"),
+               ("A.Nitrogen","B.Oxygen","C.Carbon-dioxide","D.Hydrogen"),
+               ("A.206","B.207","C.208","D.209"),
+               ("A.Mercury","B.Venus","C.Earth","D.Mars"))
+
+    answers = ("C","D","A","A","B")
+    guesses = []
+    score = 0 
+    question_num = 0
+
+    for question in questions:
+        print("-------------------------------------")
+        print(question)
+        for option in options[question_num]:
+            print(option)
+        guess = input("Enter (A,B,C,D):").upper()
+        guesses.append(guess)
+        if guess == answers[question_num]:
+            score += 1
+            print("CORRECT!")
+        else:
+            print("INCORRECT!")
+            print(f"The correct answer is {answers[question_num]}")
+        question_num += 1
+
+    print("--------------------------------")
+    print("            RESULTS             ")
+    print("--------------------------------")
+
+    print("Answers:", end="")
+    for answer in answers:
+        print(answer, end=" ")
+    print()
+
+    print("Guesses:", end="")
+    for guess in guesses:
+        print(guess, end=" ")
+    print()
+
+    score = int(score/len(questions)*100)
+    print(f"Your score is: {score}%")
+
+    play_again = input("Do you want to play again? (yes/no): ")
+
+print("Thank you for playing!")
